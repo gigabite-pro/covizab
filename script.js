ce03309c2e4688e82587a7079bf13a5b528ba042
gigabite-pro <rajmachawal19@gmail.com>
2021-07-09 18:49:48 +0530

    onload fix

--- script.js
+++ script.js
@@ -52,4 +52,14 @@ window.onload = () => {
         submitBtn.addEventListener('click', ()=>{
         alert('Form Submitted')
     })
+
+    const toTop = document.querySelector(".to-top");
+
+            window.addEventListener("scroll", () => {
+            if (window.pageYOffset > 100) {
+                toTop.classList.add("activetop");
+            } else {
+                toTop.classList.remove("activetop");
+            }
+            })
 }
